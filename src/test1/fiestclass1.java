cc440d9e180d83a3540d062ddfa3c290a306b1f6
Tanmoyray <ray.tanmoy@senrysa.com>
2016-06-15 12:10:09 +0530

    Commit2

--- src/test1/fiestclass1.java
+++ src/test1/fiestclass1.java
@@ -5,6 +5,7 @@ public class fiestclass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Test Git");
+	System.out.println("add new Git1");
 	}
 
 }
